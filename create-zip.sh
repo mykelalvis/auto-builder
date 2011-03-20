cd4b380137333e445518edfb482b12966cb92f84
james.percent <james.percent@gmail.com>
2011-03-20 00:10:46 +0000

    Clean up directory.

--- create-zip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p auto_builder/ply
-cp ply/*py auto_builder/ply 
-cp __init__.py conf.py auto_builder.py manifest.py generator.py dependencies.py auto_builder/
-zip -r auto-builder.zip auto_builder/
-rm -rf auto_builder
-
